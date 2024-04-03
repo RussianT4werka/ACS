8bf0421e0d820a493f81f2209a9da2f3c9d859fa
Александр Кокорин <sanya.kokorin.04@mail.ru>
2024-04-03 16:34:34 +1000

    Сделал создание/редактирование/удаление персонала

--- ACS_API/Controllers/PersonalsController.cs
+++ ACS_API/Controllers/PersonalsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LibraryBD.BD;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
 
 namespace ACS_API.Controllers
 {
@@ -23,17 +24,17 @@ namespace ACS_API.Controllers
         [HttpGet("GetPersonals")]
         public async Task<ActionResult<List<Personal>>> GetPersonals()
         {
-          if (_context.Personals == null)
-          {
-              return NotFound();
-          }
+            if (_context.Personals == null)
+            {
+                return NotFound();
+            }
             return await _context.Personals.ToListAsync();
         }
 
         [HttpPost("EditPersonal")]
-        public async Task<ActionResult> CreatePersonal(Personal personal)
+        public async Task<ActionResult> EditPersonal(Personal personal)
         {
-            if(personal == null)
+            if (personal == null)
             {
                 return BadRequest();
             }
@@ -57,8 +58,50 @@ namespace ACS_API.Controllers
             }
         }
 
+        [HttpPost("CreatePersonal")]
+        public async Task<ActionResult> CreatePersonal(Personal personal)
+        {
+            if (personal == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                try
+                {
+                    var Pers = new Personal() { Fio = personal.Fio, Department = personal.Department, Position = personal.Position, Hex = personal.Hex };
+                    _context.Personals.Add(Pers);
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+            }
+        }
 
-
-
+        [HttpPost("DelPers")]
+        public async Task<ActionResult> DelPers(Personal personal)
+        {
+            if (personal == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                try
+                {
+                    var editPers = _context.Personals.FirstOrDefault(s => s.Id == personal.Id);
+                    _context.Personals.Remove(editPers);
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+            }
+        }
     }
 }
