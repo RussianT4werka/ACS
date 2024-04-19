6e9ce92ca14133322e1bcb1868473e80e106249f
Александр Кокорин <sanya.kokorin.04@mail.ru>
2024-04-19 17:00:46 +1000

    Сделал добавление mjpeg потока и заканчиваю работу над отображением сетки камер видеонаблюдения

--- ACS_API/Controllers/VideoStreamsController.cs
+++ ACS_API/Controllers/VideoStreamsController.cs
@@ -31,68 +31,21 @@ namespace ACS_API.Controllers
             return await _context.VideoStreams.ToListAsync();
         }
 
-        // GET: api/VideoStreams/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<VideoStream>> GetVideoStream(int id)
-        {
-          if (_context.VideoStreams == null)
-          {
-              return NotFound();
-          }
-            var videoStream = await _context.VideoStreams.FindAsync(id);
-
-            if (videoStream == null)
-            {
-                return NotFound();
-            }
-
-            return videoStream;
-        }
-
-        // PUT: api/VideoStreams/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutVideoStream(int id, VideoStream videoStream)
-        {
-            if (id != videoStream.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(videoStream).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!VideoStreamExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
         // POST: api/VideoStreams
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<VideoStream>> PostVideoStream(VideoStream videoStream)
+        [HttpPost("AddStream")]
+        public async Task<ActionResult<VideoStream>> AddStream(VideoStream videoStream)
         {
-          if (_context.VideoStreams == null)
-          {
-              return Problem("Entity set 'AcsContext.VideoStreams'  is null.");
-          }
-            _context.VideoStreams.Add(videoStream);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetVideoStream", new { id = videoStream.Id }, videoStream);
+            try
+            {
+                _context.VideoStreams.Add(videoStream);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+               return BadRequest(ex.Message);
+            }
+            return Ok();
         }
 
         // DELETE: api/VideoStreams/5
@@ -113,11 +66,6 @@ namespace ACS_API.Controllers
             await _context.SaveChangesAsync();
 
             return NoContent();
-        }
-
-        private bool VideoStreamExists(int id)
-        {
-            return (_context.VideoStreams?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
