e5e72bef33e55eed15a225ed8f21efaa91559c4c
RussianT4werka <sanya.kokorin.04@mail.ru>
2024-06-09 00:43:49 +1000

    Изменения: 1) Сделал учёт рабочего времени с периодами; 2) Начал работу над экспортом отчёта в Excel.

--- ACS_API/Controllers/URVController.cs
+++ ACS_API/Controllers/URVController.cs
@@ -16,6 +16,8 @@ namespace ACS_API.Controllers
     {
         private readonly AcsContext _context;
         private List<URV> ListURV;
+        private DateTime DateStart;
+        private DateTime DateEnd;
 
         public URVController(AcsContext context)
         {
@@ -23,9 +25,11 @@ namespace ACS_API.Controllers
         }
 
         [HttpGet("GetListURV")]
-        public async Task<ActionResult<List<URV>>> GetListURV(string Date)
+        public async Task<ActionResult<List<URV>>> GetListURV(string dateStart, string dateEnd)
         {
             ListURV = new();
+            DateStart = Convert.ToDateTime(dateStart);
+            DateEnd = Convert.ToDateTime(dateEnd);
             try
             {
                 if (_context.Events == null)
@@ -35,22 +39,51 @@ namespace ACS_API.Controllers
                 else
                 {
                     var listPersoanl = _context.Personals.Where(s => s.Position != "Водитель АБС").ToList();
-                    foreach (var person in listPersoanl)
+                    if (DateStart == DateEnd)
                     {
-                        var startTimePerson = _context.Events.ToList().FirstOrDefault(s => s.Fio == person.Fio && Convert.ToDateTime(s.Time).Date == Convert.ToDateTime(Date).Date);
-                        var endTimePerson = _context.Events.ToList().LastOrDefault(s => s.Fio == person.Fio && Convert.ToDateTime(s.Time).Date == Convert.ToDateTime(Date).Date);
-                        if (startTimePerson != null && endTimePerson != null)
+                        foreach (var person in listPersoanl)
                         {
-                            DateTime startTime = Convert.ToDateTime(startTimePerson.Time);
-                            DateTime endTime = Convert.ToDateTime(endTimePerson.Time);
+                            var startTimePerson = _context.Events.ToList().FirstOrDefault(s => s.Fio == person.Fio && Convert.ToDateTime(s.Time).Date == DateStart.Date);
+                            var endTimePerson = _context.Events.ToList().LastOrDefault(s => s.Fio == person.Fio && Convert.ToDateTime(s.Time).Date == DateStart.Date);
+                            if (startTimePerson != null && endTimePerson != null)
+                            {
+                                DateTime startTime = Convert.ToDateTime(startTimePerson.Time);
+                                DateTime endTime = Convert.ToDateTime(endTimePerson.Time);
 
-                            TimeSpan start = startTime.TimeOfDay;
-                            TimeSpan end = endTime.TimeOfDay;
+                                TimeSpan start = startTime.TimeOfDay;
+                                TimeSpan end = endTime.TimeOfDay;
 
-                            TimeSpan totalTime = end - start;
+                                TimeSpan totalTime = end - start;
 
-                            var urv = new URV() { Date = Convert.ToDateTime(Date), FIO = startTimePerson.Fio, Position = startTimePerson.Position, StartTime = startTime.TimeOfDay, EndTime = endTime.TimeOfDay, TotalTime = totalTime };
-                            ListURV.Add(urv);
+                                var urv = new URV() { Date = Convert.ToDateTime(DateStart), FIO = startTimePerson.Fio, Position = startTimePerson.Position, StartTime = startTime.TimeOfDay, EndTime = endTime.TimeOfDay, TotalTime = totalTime };
+                                ListURV.Add(urv);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        
+                        while (DateStart <= DateEnd)
+                        {
+                            foreach (var person in listPersoanl)
+                            {
+                                var startTimePerson = _context.Events.ToList().FirstOrDefault(s => s.Fio == person.Fio && Convert.ToDateTime(s.Time).Date == DateStart.Date);
+                                var endTimePerson = _context.Events.ToList().LastOrDefault(s => s.Fio == person.Fio && Convert.ToDateTime(s.Time).Date == DateStart.Date);
+                                if (startTimePerson != null && endTimePerson != null)
+                                {
+                                    DateTime startTime = Convert.ToDateTime(startTimePerson.Time);
+                                    DateTime endTime = Convert.ToDateTime(endTimePerson.Time);
+
+                                    TimeSpan start = startTime.TimeOfDay;
+                                    TimeSpan end = endTime.TimeOfDay;
+
+                                    TimeSpan totalTime = end - start;
+
+                                    var urv = new URV() { Date = Convert.ToDateTime(DateStart), FIO = startTimePerson.Fio, Position = startTimePerson.Position, StartTime = startTime.TimeOfDay, EndTime = endTime.TimeOfDay, TotalTime = totalTime };
+                                    ListURV.Add(urv);
+                                }
+                            }
+                            DateStart += new TimeSpan(1, 0, 0, 0);
                         }
                     }
                     return Ok(ListURV);
@@ -60,6 +93,13 @@ namespace ACS_API.Controllers
             {
                 return BadRequest(ex.Message);
             }
+        }
+
+        [HttpPost("CreateReportExcel")]
+        public async Task<ActionResult> CreateReportExcel()
+        {
+
+            return Ok();
         }
     }
 }
