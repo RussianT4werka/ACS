3c5416c42fb3a7b4c04d4bd15a7f4033f7f6841f
RussianT4werka <sanya.kokorin.04@mail.ru>
2024-06-10 23:43:52 +1000

    Изменения: 1) Исправил баг с открытием через раз выпадающего списка на странице видеонаблюдения(координально его переделал; 2) Поменял градиент navbar'а; 3) Сделал выгрузку excel файла по базовому адресу(чтобы работала с внешнего ip). 4) Общие правки.

--- ACS_API/Controllers/OffendersController.cs
+++ ACS_API/Controllers/OffendersController.cs
@@ -75,7 +75,7 @@ namespace ACS_API.Controllers
             {
                 newListOffender = new();
                 var newListEvents = _context.Events.ToList().Where(s => s.PassOrDeny == "DENY" && s.SendOrNot == 0);
-                var deadLine = new TimeSpan(0, 5, 00);
+                var deadLine = new TimeSpan(0, 15, 00);
                 var newListCycle = _context.Cycles.ToList().Where(s => s.Delta > deadLine && s.SendOrNot == 0);
                 if (newListEvents.Count() != 0 || newListEvents != null)
                 {
